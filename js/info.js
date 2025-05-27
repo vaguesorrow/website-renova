e998c6136ab9292dfba63e6e4257b5702f9e2fc5
alina m <alinamocalova451@gmail.com>
2025-05-27 19:59:01 +0300

    bebebe

---
+++ js/info.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const bluesph1 = document.querySelector('bluesphere-ques-info-1');
+    const whitestr1= document.querySelector('white-str-info-1');
+
+    bluesph1.addEventListener('mouseover', function() {
+        whitestr1.style.display = 'flex';
+        whitestr1.classList.add('fullscreen');
+    });
+});
